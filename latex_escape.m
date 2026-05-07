a9becfecd5fa24ba25a8af5cded3c877e5f18f70
Behrooz Moosavi <BEM159@pitt.edu>
2026-05-06 23:17:14 -0400

    Delete latex_escape.m
    Signed-off-by: Behrooz Moosavi  <BEM159@pitt.edu>

--- latex_escape.m
+++
@@ -1,8 +0,0 @@
-function out = latex_escape(s)
-%% latex_escape.m  |  FILE 14 of 30
-%  Escapes _ % & for safe use in LaTeX table cells.
-    out = string(s);
-    out = replace(out, "_", "\\_");
-    out = replace(out, "%", "\\%");
-    out = replace(out, "&", "\\&");
-end
