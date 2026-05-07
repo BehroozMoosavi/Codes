ba7e54ea5a0fe9f156c29b14f03d5e77a450291c
Behrooz Moosavi <BEM159@pitt.edu>
2026-05-06 23:13:05 -0400

    Delete back_transform.m
    Signed-off-by: Behrooz Moosavi  <BEM159@pitt.edu>

--- back_transform.m
+++
@@ -1,9 +0,0 @@
-function [yhat_level, yhat_z_raw] = back_transform(yhat_z_std, y_lag1, z_mean, z_std)
-%% back_transform.m  |  FILE 12 of 30
-%  Un-standardize z_hat and invert the log-change transformation.
-%  Step 1: z_raw  = z_mean + z_std * z_std_hat
-%  Step 2: y_hat  = y_{t-1} * exp(z_raw / 100)
-
-    yhat_z_raw = z_mean + z_std .* yhat_z_std;
-    yhat_level = y_lag1 .* exp(yhat_z_raw / 100);
-end
